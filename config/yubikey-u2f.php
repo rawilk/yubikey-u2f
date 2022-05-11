b0ebe9f182bb84031785f6271113d7fecfb1be43
Randall Wilk <wilk.randall@gmail.com>
2022-05-11 10:16:34 -0500

    Add basic config

--- config/yubikey-u2f.php
+++ config/yubikey-u2f.php
@@ -1,5 +1,15 @@
 <?php
 
 return [
+    'client_id' => env('YUBIKEY_CLIENT_ID'),
 
+    'secret_key' => env('YUBIKEY_SECRET_KEY'),
+
+    'urls' => [
+        'api.yubico.com/wsapi/2.0/verify',
+        'api2.yubico.com/wsapi/2.0/verify',
+        'api3.yubico.com/wsapi/2.0/verify',
+        'api4.yubico.com/wsapi/2.0/verify',
+        'api5.yubico.com/wsapi/2.0/verify',
+    ],
 ];
